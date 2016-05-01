0ae5133e241da284f9535871a189e0808b846dec
Festus Yeboah <bretuobay@gmail.com>
2016-05-01 02:07:40 +0200

    added json functions

--- text_splitter.py
+++ text_splitter.py
@@ -1,13 +1,16 @@
 from collections import defaultdict
+import json
 
 
 class FileSplitter(object):
 
     def __init__(self,txt_file_name):
+
         self.data = txt_file_name
 
 
     def split_text(self):
+
         line_data = defaultdict(list)
         with open(self.data, 'r') as infile:
              for ind, line in enumerate(infile):
@@ -26,10 +29,20 @@ class FileSplitter(object):
 
         return elem
 
+    def json_record(self,index):
+
+        return json.dumps(self.get_record(index))
+
+
+    def json_records(self):
+
+         return json.dumps(self.get_data_for_db())
+
 
     def get_data_for_db(self):
 
          content = res =  defaultdict(list)
+
          content = self.split_text()
          for i, val in enumerate(content):
              res[i] = self.get_record(i)
@@ -39,5 +52,5 @@ class FileSplitter(object):
 
 if __name__ == "__main__":
     file_obj = FileSplitter('ANCLI_000024_2015.TXT')
-    print file_obj.get_data_for_db()
+    print file_obj.json_records()
     #print file_obj.get_record(0)
